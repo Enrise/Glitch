17c456574acb4a58c29b4fa6ad07cf1137075f76
Dolf Schimmel <dolfschimmel@gmail.com>
2011-05-19 10:57:48 +0200

    Added gitsvn 'docs'

---
+++ tests/runtests.sh
@@ -0,0 +1,3 @@
+phpunit  --testdox-html ../public/coverage/testdox.html --coverage-html ../public/coverage --configuration phpunit-local.xml --verbose --colors --bootstrap phpunit-boot.php $1
+#phpunit --coverage-html ../public/coverage --configuration phpunit.xml --verbose --colors application/modules/Account
+
