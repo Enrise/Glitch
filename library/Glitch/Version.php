396513ca037ce4f9fd5fcee6e908267a759f0053
Dolf Schimmel <dolfschimmel@gmail.com>
2011-07-25 20:24:51 +0200

    Prepping for release 2.0.0

---
+++ library/Glitch/Version.php
@@ -0,0 +1,26 @@
+<?php
+
+final class Glitch_Version
+{
+    /**
+     * Zend Framework version identification - see compareVersion()
+     */
+    const VERSION = '2.0.0';
+
+    /* Compare the specified Zend Framework version string $version
+     * with the current Zend_Version::VERSION of Zend Framework.
+     *
+     * @param  string  $version  A version string (e.g. "0.7.1").
+     * @return int           -1 if the $version is older,
+     *                           0 if they are the same,
+     *                           and +1 if $version is newer.
+     *
+     */
+    public static function compareVersion($version)
+    {
+        $version = strtolower($version);
+        $version = preg_replace('/(\d)pr(\d?)/', '$1a$2', $version);
+        return version_compare($version, strtolower(self::VERSION));
+    }
+
+}
