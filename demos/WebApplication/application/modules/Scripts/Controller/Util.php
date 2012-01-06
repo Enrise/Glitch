4c3b5e0a77b7f98f2ce16933a3068f9eca3c8581
stefanooldeman <stefano.oldeman@gmail.com>
2012-01-06 15:16:21 +0100

    add demo web application

---
+++ demos/WebApplication/application/modules/Scripts/Controller/Util.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Controller for running utility scripts
+ *
+ */
+class Scripts_Controller_Util extends Zend_Controller_Action
+{
+    /**
+     * Initializes the controller
+     *
+     * @return void
+     * @throws Glitch_Exception
+     */
+    public function init()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+
+        // Make sure we're being called as a CLI script
+        if (php_sapi_name() != 'cli') {
+            throw new Glitch_Exception('Bad request');
+        }
+    }
+
+    /**
+     * Disable method; not in use
+     *
+     * @return void
+     * @throws Glitch_Exception
+     */
+    public function indexAction()
+    {
+        throw new Glitch_Exception('Bad action');
+    }
+
+    /**
+     * Test this by calling cli/index.php -r scripts.util.example -e <environment>
+     *
+     * @return void
+     */
+    public function exampleAction()
+    {
+        echo 'It works!';
+    }
+}
